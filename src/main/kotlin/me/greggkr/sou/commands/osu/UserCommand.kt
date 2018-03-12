7ae80c1b6d3d238b1300bfdb0ba3bb9a5cf4d43f
Gregg <greggkrzykowski@gmail.com>
2018-03-11 20:54:13 -0500

    profile picture + basic stats

--- src/main/kotlin/me/greggkr/sou/commands/osu/UserCommand.kt
+++ src/main/kotlin/me/greggkr/sou/commands/osu/UserCommand.kt
@@ -5,28 +5,45 @@ import me.diax.comportment.jdacommand.CommandDescription
 import me.greggkr.sou.Sou
 import me.greggkr.sou.osu.Mode
 import me.greggkr.sou.osu.wrappers.User
+import me.greggkr.sou.util.Data
 import me.greggkr.sou.util.Emoji
+import net.dv8tion.jda.core.EmbedBuilder
 import net.dv8tion.jda.core.entities.Message
+import java.text.DecimalFormat
 
 @CommandDescription(name = "user", triggers = [
     "user", "u"
 ], description = "Gets information about an osu! user.")
 class UserCommand : Command {
+    private val decFormat = DecimalFormat("######.###")
     override fun execute(message: Message, a: String) {
         if (a.isEmpty()) {
-            message.channel.sendMessage("${Emoji.X} **Correct usage: user <name>.**").queue()
+            message.channel.sendMessage("${Emoji.X} **Correct usage: s!user <name>.**").queue()
             return
         }
 
-        val name = a.split(Regex("\\s+"))[0]
+        val avatar = Sou.osu.getProfilePicture(a)
 
-        val user: User? = Sou.osu.getUser(Mode.STANDARD, name)
+        if (avatar == null) {
+            message.channel.sendMessage("user doesnt exist or doesnt have profile pic").queue()
+            return
+        }
+
+        val user: User? = Sou.osu.getUser(Mode.STANDARD, a)
 
         if (user == null) {
             message.channel.sendMessage("${Emoji.X} **User not found.**").queue()
             return
         }
 
-        message.channel.sendMessage("rank: ${user.rank}").queue()
+        message.channel.sendMessage(EmbedBuilder()
+                .setColor(Data.color)
+                .setThumbnail(Sou.osu.getProfilePicture(a))
+                .addField("Info", "Name: ${user.name}\n" +
+                        "PP: ${decFormat.format(user.pp)}\n" +
+                        "Rank: ${user.rank}\n" +
+                        "Rank in ${user.country}: ${user.countryRank}\n" +
+                        "Acc: ${decFormat.format(user.accuracy)}%", true)
+                .build()).queue()
     }
 }
